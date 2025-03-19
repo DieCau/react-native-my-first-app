52d749424abf31753111b5ebfd8437cc0dce35ac
g <diegocaucota@gmail.com>
2025-03-19 00:03:11 -0300

    add: component image to App

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { homeStyles } from './styles/HomeStyles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{color: 'white'}}>Hola me llamo Diego!</Text>
-      <StatusBar style="auto" />
+    <View style={homeStyles.container}>
+      <View>
+        <Text style={homeStyles.titulo}>Este es el título</Text>
+      </View>
+      <Text>Hola me llamo Diego!</Text>
+      <Image source={{ uri: 'https://www.infobae.com/resizer/v2/IRE5TJ2RXRFFXEQBIDCUEVORAM.jpg?auth=6d100eb097084544152f4b32bc6ffd3b099a346234206c2397a652fd7dbd4788&smart=true&width=1200&height=1200&quality=85'  }} 
+      height={300} width={300}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1a5dbc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
---
+++ styles/HomeStyles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const homeStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1a5dbc',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 700,
+  },
+})
